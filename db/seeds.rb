81ce7d4bfb0a03bd1f8ef1fe1d294640070e249d
zhigs <geoffrey.ramler@gmail.com>
2018-01-02 01:44:40 -0800

    Add email authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,25 @@ User.create!(name:  "Geoff Ramler",
              email: "geoffrey.ramler@gmail.com",
              password:              "Fri$cit!6grunt",
              password_confirmation: "Fri$cit!6grunt",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
-99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n+1}@example.com"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
+User.create!(name:  "Example User",
+             email: "user@example.com",
+             password:              "password",
+             password_confirmation: "password",
+             activated: false,
+             activated_at: Time.zone.now)
+
+#99.times do |n|
+#  name = Faker::Name.name
+#  email = "example-#{n+1}@example.com"
+#  password = "password"
+#  User.create!(name: name,
+#               email: email,
+#               password:              password,
+#               password_confirmation: password,
+#               activated: true,
+#               activated_at: Time.zone.now)
+#end
