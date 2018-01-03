249396b9d8247e0c9e01a6378c7b912d488a23ae
zhigs <geoffrey.ramler@gmail.com>
2018-01-03 02:38:27 -0800

    Refactor users index properly and add dummy users back

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,14 @@ User.create!(name:  "Example User",
              activated: false,
              activated_at: Time.zone.now)
 
-#99.times do |n|
-#  name = Faker::Name.name
-#  email = "example-#{n+1}@example.com"
-#  password = "password"
-#  User.create!(name: name,
-#               email: email,
-#               password:              password,
-#               password_confirmation: password,
-#               activated: true,
-#               activated_at: Time.zone.now)
-#end
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@example.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
