c8867bc911126ae08289a9934e72af020d5e7607
zhigs <geoffrey.ramler@gmail.com>
2018-01-07 00:12:29 -0800

    Update postbox and add follow buttons to feeds

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,13 @@
 class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
+  include ActionView::Helpers::UrlHelper
+
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
     respond_to do |format|
-      format.html { redirect_to @user }
+      format.html { redirect_back(fallback_location: '/') }
       format.js
     end
   end
@@ -14,7 +16,7 @@ class RelationshipsController < ApplicationController
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
     respond_to do |format|
-      format.html { redirect_to @user }
+      format.html { redirect_back(fallback_location: '/') }
       format.js
     end
   end
