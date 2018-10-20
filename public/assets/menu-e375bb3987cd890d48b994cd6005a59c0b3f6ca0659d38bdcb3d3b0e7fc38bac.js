74b4f1f4e4b869330866a28dc8dc9b5daac09ce7
Geoff Ramler <geoffrey.ramler@gmail.com>
2018-10-19 22:18:07 -0700

    Animate menu

---
+++ public/assets/menu-e375bb3987cd890d48b994cd6005a59c0b3f6ca0659d38bdcb3d3b0e7fc38bac.js
@@ -0,0 +1,20 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.height = "40px";
+    document.getElementById("navbar").style.padding = "0px";
+    document.getElementById("logotext").style.fontSize = "0px";
+    document.getElementById("dropdown-toggle").style.height = "50px";
+    document.getElementById("dropdown").style.height = "50px";
+    document.getElementById("navbar").style.opacity = ".9";
+  } else {
+    document.getElementById("navbar").style.height = "70px";
+    document.getElementById("navbar").style.padding = "10px";
+    document.getElementById("logotext").style.fontSize = "1.7em";
+    document.getElementById("dropdown-toggle").style.height = "60px";
+    document.getElementById("dropdown").style.height = "60px";
+    document.getElementById("navbar").style.opacity = "1";
+  }
+}
+;
