01f2aadc05d7386fb2dc34bdd5087482bf16d632
Geoff <geoff@ramler.net>
2019-08-19 17:46:53 -0700

    Fix mobile view

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,25 +1,28 @@
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    document.getElementById("navbar").style.height = "40px";
-    document.getElementById("navbar").style.padding = "0px";
-    document.getElementById("logotext").style.fontSize = "0px";
-    document.getElementById("dropdown-toggle").style.height = "50px";
-    document.getElementById("dropdown").style.height = "50px";
-    document.getElementById("navbar").style.opacity = ".9";
-    document.getElementById("logoimage").style.maxHeight = "32px";
-    document.getElementById("logoimage").style.marginTop = "10px";
-    document.getElementById("alpha-menu").style.marginLeft = "-10px";
-  } else {
-    document.getElementById("navbar").style.height = "70px";
-    document.getElementById("navbar").style.padding = "10px";
-    document.getElementById("logotext").style.fontSize = "1.7em";
-    document.getElementById("dropdown-toggle").style.height = "60px";
-    document.getElementById("dropdown").style.height = "60px";
-    document.getElementById("navbar").style.opacity = "1";
-    document.getElementById("logoimage").style.maxHeight = "40px";
-    document.getElementById("logoimage").style.marginTop = "5px";
-    document.getElementById("alpha-menu").style.marginLeft = "0px";
+if($(window).width() >= 769){
+  function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+      document.getElementById("navbar").style.height = "40px";
+      document.getElementById("navbar").style.padding = "0px";
+      document.getElementById("logotext").style.fontSize = "0px";
+      document.getElementById("dropdown-toggle").style.height = "50px";
+      document.getElementById("dropdown").style.height = "50px";
+      document.getElementById("navbar").style.opacity = ".9";
+      document.getElementById("logoimage").style.maxHeight = "32px";
+      document.getElementById("logoimage").style.marginTop = "10px";
+      document.getElementById("alpha-menu").style.marginLeft = "-10px";
+    } else {
+      document.getElementById("navbar").style.height = "70px";
+      document.getElementById("navbar").style.padding = "10px";
+      document.getElementById("logotext").style.fontSize = "1.7em";
+      document.getElementById("dropdown-toggle").style.height = "60px";
+      document.getElementById("dropdown").style.height = "60px";
+      document.getElementById("navbar").style.opacity = "1";
+      document.getElementById("logoimage").style.maxHeight = "40px";
+      document.getElementById("logoimage").style.marginTop = "5px";
+      document.getElementById("alpha-menu").style.marginLeft = "0px";
+    }
   }
 }
+
